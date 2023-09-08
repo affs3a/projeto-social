de60aac35e046f2e3a8f77899ddcfe8f48ff07a5
Aparecido <Aparecido.silva12y@gmail.com>
2023-09-08 17:49:56 -0300

    i started navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
+import NavBar from "./components/navegation/NavBar"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  *, body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
 
 const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <GlobalStyle />
+      <NavBar />
     </>
   )
 }
