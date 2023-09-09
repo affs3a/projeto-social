ec93446df39a566e0e6e3d7c0c132ce27eec3338
Aparecido <Aparecido.silva12y@gmail.com>
2023-09-08 21:42:07 -0300

    Revert "i started navbar"
    This reverts commit de60aac35e046f2e3a8f77899ddcfe8f48ff07a5.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,8 @@
-import NavBar from "./components/navegation/NavBar"
-import { createGlobalStyle } from "styled-components"
-
-const GlobalStyle = createGlobalStyle`
-  *, body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
 
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <NavBar />
+      <h1>Hello world</h1>
     </>
   )
 }
