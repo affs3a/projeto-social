1f8cbbe3f23ea445f5de7b43d0e3fd398271e17c
Renan <alvesrenanpro@gmail.com>
2023-09-11 00:24:18 -0300

    feat(users): initial unit tests for users API

--- server/apps/users/tests.py
+++ server/apps/users/tests.py
@@ -1,3 +1,35 @@
+from collections import OrderedDict
 from django.test import TestCase
+from rest_framework.utils.serializer_helpers import ReturnList
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from .models import User
+from . import views
 
-# Create your tests here.
+
+class UserTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(
+            name='Renan',
+            username='renan',
+            password='renan',
+            email='renan@test.com',
+            role=3,
+        )
+
+    def setUp(self):
+        print("--> Setup: created test database.")
+        self.client = APIRequestFactory()
+
+    def test_get_all_users(self):
+        print("--> HTTP(GET) api/users: get all users.")
+        request = self.client.get('/api/users/')
+        view = views.UserList.as_view()
+        response = view(request)
+
+        print(response.data)
+
+        self.assertTrue(type(response.data) == ReturnList)
+        self.assertIsNotNone(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
