a65747f8b53e2028f5984a73569e7d21db403d6e
Renan <alvesrenanpro@gmail.com>
2023-12-15 14:10:27 -0300

    feat: class to make the defalt route

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .defaults import views as default_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', default_view.wellcome, name='api'),
     path('api/users/', include('apps.users.urls')),
     path('api/auth/', include('apps.authentication.urls')),
     path('api/categories/', include('apps.categories.urls')),
