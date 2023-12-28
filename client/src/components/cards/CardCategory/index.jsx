108d5f6deba18fb38553e60d3b11ac0870cde687
Aparecido Ribeiro <aparecido.silva12y@gmail.com>
2023-12-28 14:21:33 -0300

    feat: finalizei o card das categorias

--- client/src/components/cards/CardCategory/index.jsx
+++ client/src/components/cards/CardCategory/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const Div = styled(Link)`
     width: 100%;
     max-width: 330px;
-    min-width: 300px;
+    min-width: 280px;
     height: auto;
     border-radius: 10px;
     display: grid;
@@ -14,6 +14,7 @@ const Div = styled(Link)`
     border-radius: 6px;
     border: 1px solid rgba(30, 30, 30, 0.22);
     box-shadow: 2px 2px 4px 0px rgba(0, 0, 0, 0.25);
+    grid-template-columns: repeat(3, 1fr);
     grid-template-areas: 
         "image category category"
         "image number number"
@@ -29,7 +30,8 @@ const Image = styled.img`
     grid-area: image;
     grid-column: image;
     align-self: center;
-    margin-right: 2rem;
+
+
 `
 const Category = styled.h2`
     font-size: 20px;
@@ -45,6 +47,7 @@ const Number = styled.h3`
     grid-area: number;
     font-size: 16px;
     align-self: center;
+    text-align: center;
 `
 
 const CardCategory = ({ Name, number, endereco }) => {
